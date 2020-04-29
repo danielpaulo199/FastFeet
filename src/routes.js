fdee23943df82a520a305901380374a9aa5b582e
Daniel Paulo Dos Santos <daniel223618@br-atacadao.corp>
2020-04-29 10:11:39 -0300

    Add: Token auth Middleware

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import AuthMiddleware from './app/middlewares/auth';
 
 const routes = Router();
 
 routes.post('/login', SessionController.store);
 
+routes.use(AuthMiddleware);
 routes.get('/recipients', RecipientController.show);
 routes.post('/recipients/create', RecipientController.store);
 
