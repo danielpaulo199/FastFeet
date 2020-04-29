e24661c23ff2daf2d2235f61f2bc3a9efd2ded7d
Daniel Paulo Dos Santos <daniel223618@br-atacadao.corp>
2020-04-29 10:46:02 -0300

    Add: Data validation on recipient create

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            rua: Yup.string().required(),
+            numero: Yup.number().required(),
+            complemento: Yup.string().required(),
+            estado: Yup.string().required(),
+            cidade: Yup.string().required(),
+            cep: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({ error: 'Validations data failed!' });
+        }
         const {
             name,
             rua,
